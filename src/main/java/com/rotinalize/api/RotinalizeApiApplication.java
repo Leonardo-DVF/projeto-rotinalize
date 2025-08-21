ced6afebb44dabee59bb45237fc14fffdb71494c
Léo Duarte <leonardoveiga010@gmail.com>
2025-08-21 19:32:25 -0300

    inicia projeto

---
+++ src/main/java/com/rotinalize/api/RotinalizeApiApplication.java
@@ -0,0 +1,13 @@
+package com.rotinalize.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotinalizeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotinalizeApiApplication.class, args);
+	}
+
+}
